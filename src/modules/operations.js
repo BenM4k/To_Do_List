6f80b48da8ed0b115861c09dcbe6af7512865cad
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-04 10:49:26 +0200

    Update operations.js

--- src/modules/operations.js
+++ src/modules/operations.js
@@ -10,9 +10,9 @@ export function addTask(description, tasks) {
 
 export function deleteTask(index, tasks) {
   tasks.splice(index - 1, 1);
-  tasks.forEach((task, i) => (task.index = i + 1));
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-};
+  tasks.forEach((task, i) => { task.index = i + 1; });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 export function editTask(index, newDescription, tasks) {
   tasks[index].description = newDescription;
